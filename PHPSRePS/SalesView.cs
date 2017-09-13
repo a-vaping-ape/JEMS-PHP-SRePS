dfb3983f5e0f4cce6b5c927deb7ee76cc53e5118
Yohoo2333 <31644549+Yohoo2333@users.noreply.github.com>
2017-09-13 15:49:47 +1000

    Update SalesView.cs

--- PHPSRePS/SalesView.cs
+++ PHPSRePS/SalesView.cs
@@ -10,12 +10,30 @@ using System.Windows.Forms;
 
 namespace PHPSRePS {
     public partial class SalesView : Form {
+        List<Sales> salesList = new List<Sales>();
+            
         public SalesView() {
             InitializeComponent();
+            LoadSales();
         }
 
         private void LoadSales() {
-
+            string query = "select * from t_user";  
+            MySqlConnection myConnection = new MySqlConnection("server=XX;user id=XX;password=XX;database=XX");  
+            MySqlCommand myCommand = new MySqlCommand(query, myConnection);  
+            myConnection.Open();  
+            myCommand.ExecuteNonQuery();  
+            MySqlDataReader myDataReader = myCommand.ExecuteReader();  
+            string XX = "";  
+            while (myDataReader.Read() == true)  
+            {  
+                sales.id = 0;
+                sales.date ="";
+                sales.employee ="";
+                
+                saleslist.add(product);
+            }
+   
         }
 
         private void DisplayAllSales() {
