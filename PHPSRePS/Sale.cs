aef0335c35a50a6e7bffd6c1e5c59781d5ecd4b2
Yohoo2333 <31644549+Yohoo2333@users.noreply.github.com>
2017-09-13 15:11:09 +1000

    Update Sale.cs

--- PHPSRePS/Sale.cs
+++ PHPSRePS/Sale.cs
@@ -5,6 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PHPSRePS {
-    class Sale {
+   public class Sale {
+       private int _id;
+       private string _date;
+       private string _employee;
+       
+       public Sale(){
+       }
+       
+       public int Id{
+         get => _id;
+         get => _id = value;          
+       }
+       
+       public string Date{
+         get => _date;
+         get => _date = value;   
+       }
+       
+       public string Employee{
+         get => _employee;
+         get => _employee = value;   
+       }
     }
 }
