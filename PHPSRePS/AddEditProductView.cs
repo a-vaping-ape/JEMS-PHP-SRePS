e2be34ae67aa7ed354540a6188e1c9b8dd45888f
a-vaping-ape <jason.97.liew@gmail.com>
2017-09-14 19:49:09 +1000

    update methods for adding or editing

--- PHPSRePS/AddEditProductView.cs
+++ PHPSRePS/AddEditProductView.cs
@@ -11,50 +11,60 @@ using System.Windows.Forms;
 namespace PHPSRePS {
     public partial class AddEditProductView : Form {
         // the product to be added or edited
-        Product workingProduct = new Product();
+        Product currentProduct = new Product();
 
         // main constructor
         public AddEditProductView(int productId) {
             InitializeComponent();
 
-            workingProduct.Id = productId;
+            currentProduct.Id = productId;
 
-            if (workingProduct.Id == 0) {
-                Add();
-            } else {
-                Edit();
+            if (currentProduct.Id != 0) {
+                PopulateFields();
             }
         }
 
-        private void Add() {
+        // save the info provided by the user in the fields into the database as a new entry
+        private void SaveNewEntry() {
             // add code to connect to database here
 
-            // populate with user input
-            workingProduct.Name = "";
-            workingProduct.Category = "";
-            workingProduct.Price = 0;
-            workingProduct.Stock = 0;
-            workingProduct.Discontinued = false;
+            // populate with user input from UI
+            currentProduct.Name = "";
+            currentProduct.Category = "";
+            currentProduct.Price = 0;
+            currentProduct.Stock = 0;
+            currentProduct.Discontinued = false;
 
-            // add code to send data to database here as a new entry
+            // add code to send data to database here as a new entry using data in currentProduct
+
         }
 
-        private void Edit() {
+        // pre-populate the fields using data from the database associated with the selected productID
+        private void PopulateFields() {
             // add code to connect to database here
 
             // search database table for product with the supplied productId and populate object fields with the product's data
-            workingProduct.Name = "";
-            workingProduct.Category = "";
-            workingProduct.Price = 0;
-            workingProduct.Stock = 0;
-            workingProduct.Discontinued = false;
+            currentProduct.Name = "";
+            currentProduct.Category = "";
+            currentProduct.Price = 0;
+            currentProduct.Stock = 0;
+            currentProduct.Discontinued = false;
 
-            // add code here to fill textfields with loaded data inside toAddEdit
+            // add code here to fill textfields in the UI with loaded data inside currentProduct (update entry with the ID of currentProduct.Id)
 
-            // add code to update data in the database here
         }
 
-        private void Delete() {  // not a requirement but you can add this feature if you want
+        // update the info provided by the user in the fields into the database
+        private void SaveEdits() {
+            // add code to connect to database here
+
+            // add code to update data in the database here using data in currentProduct
+
+        }
+
+        // delete the current product from the database
+        // not a requirement but you can add this feature if you want
+        private void Delete() {  
             // add code to connect to database here
 
             // add code to delete entry from the database here
--- PHPSRePS/AddEditSaleView.cs
+++ PHPSRePS/AddEditSaleView.cs
@@ -11,46 +11,55 @@ using System.Windows.Forms;
 namespace PHPSRePS {
     public partial class AddEditSaleView : Form {
         // the sale to be added or edited
-        Sale workingSale = new Sale();
-        
+        Sale currentSale = new Sale();
+
         // main constructor
         public AddEditSaleView(int saleID) {
             InitializeComponent();
 
-            workingSale.Id = saleID;
-            
-            if (workingSale.Id == 0) {
-                Add();
-            } else {
-                Edit();
+            currentSale.Id = saleID;
+
+            if (currentSale.Id != 0) {
+                PopulateFields();
             }
         }
 
-        private void Add() {
+        // save the info provided by the user in the fields into the database as a new entry
+        private void SaveNewEntry() {
             // add code to connect to database here
 
-            // populate with user input
-            workingSale.Date = null;
-            workingSale.Employee = null;
+            // populate with user input from UI
+            currentSale.Date = null;
+            currentSale.Employee = null;
 
-            // add code to send data to database here as a new entry
+            // add code to send data to database here as a new entry using data in currentSale
+
         }
 
-        private void Edit(){
+        // pre-populate the fields using data from the database associated with the selected saleID
+        private void PopulateFields() {
             // add code to connect to database here
 
-            // populate with user input
-            workingSale.Date = null;
-            workingSale.Employee = null;
+            // search database table for sale with the supplied saleId and populate object fields with the product's data
+            currentSale.Date = null;
+            currentSale.Employee = null;
 
-            // add code here to fill textfields with loaded data inside toAddEdit
-
-            // add code to update data in the database here
+            // add code here to fill textfields in the UI with loaded data inside currentSale (update entry with the ID of currentSale.Id)
         }
 
-        private void Delete() {  // not a requirement but you can add this feature if you want
+        // update the info provided by the user in the fields into the database
+        private void SaveEdits() {
             // add code to connect to database here
-            
+
+            // add code to update data in the database here using data in currentSale
+
+        }
+
+        // delete the current sale from the database
+        // not a requirement but you can add this feature if you want
+        private void Delete() {  
+            // add code to connect to database here
+
             // add code to delete entry from the database here
         }
 
