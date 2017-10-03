ed7981bd1e5d29a34c68c4c842c2bb0176b97505
Yohoo2333 <31644549+Yohoo2333@users.noreply.github.com>
2017-10-04 10:46:51 +1100

    Add files via upload

---
+++ PHPSRePS/backup.cs
@@ -0,0 +1,61 @@
+        private void btnBak_Click(object sender, EventArgs e) //backup
+        {
+            string saveAway = this.tbxBakLoad.Text.ToString().Trim();
+            string cmdText = @"backup database " + System.Configuration.ConfigurationSettings.AppSettings["dbName"] + " to disk='" + saveAway + "'";
+            BakReductSql(cmdText,true);            
+        }
+        private void btnReduct_Click(object sender, EventArgs e)  //recover
+        {
+            string openAway = this.tbxReductLoad.Text.ToString().Trim();//read the path
+            string cmdText = @"restore database " + System.Configuration.ConfigurationSettings.AppSettings["dbName"] + " from disk='" + openAway + "'";            
+            BakReductSql(cmdText,false);
+        }
+        /// <summary>
+        /// backup and recover database via sql
+        /// </summary>
+        /// <param name="cmdText">backup or recover</param>
+        /// <param name="isBak">it is for backup，true or false</param>
+        private void BakReductSql(string cmdText,bool isBak)
+        {
+            SqlCommand cmdBakRst = new SqlCommand();
+            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=master;uid=sa;pwd=;");
+            try
+            {
+                conn.Open();
+                cmdBakRst.Connection = conn;
+                cmdBakRst.CommandType = CommandType.Text;
+                if (!isBak)     //recover
+                {
+                    string setOffline = "Alter database GroupMessage Set Offline With rollback immediate ";
+                    string setOnline = " Alter database GroupMessage Set Online With Rollback immediate";
+                    cmdBakRst.CommandText = setOffline + cmdText + setOnline;
+                }
+                else
+                {
+                    cmdBakRst.CommandText = cmdText;
+                }
+                cmdBakRst.ExecuteNonQuery();
+                if (!isBak)
+                {
+                    MessageBox.Show("the data is recovered", "System message");
+                }
+                else
+                {
+                    MessageBox.Show("You have backup the data", "System message");
+                }
+            }
+            catch (SqlException sexc)
+            {
+                MessageBox.Show("fail" + sexc, "error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("fail：" + ex, "error");
+            }
+            finally
+            {
+                cmdBakRst.Dispose();
+                conn.Close();
+                conn.Dispose();
+            }
+        }
